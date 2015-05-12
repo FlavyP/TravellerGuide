5b6f642eb3c0e3788370a0315580a9e52742787b
Flavi <Flavi@10.52.228.153>
2015-05-12 11:51:21 +0200

    Added setters and getters to Hotel

--- src/model/Hotel.java
+++ src/model/Hotel.java
@@ -8,7 +8,14 @@ public class Hotel
    private String name;
    private String address;
    private String city;
-    
+   private int numberOfSingleRooms;
+   private double singleRoomPrice;
+   private int numberOfDoubleRooms;
+   private double doubleRoomPrice;
+   private int numberOfTripleRooms;
+   private double tripleRoomPrice;
+   private int numberOfApartments;
+   private double apartmentPrice;
    //create private parameters for each number of room
 
    public Hotel(String name, String address, String city, 
@@ -21,10 +28,6 @@ public class Hotel
       this.name = name;
       this.address = address;
       this.city = city;
-      addRoom(new SingleRoom(singleRoomPrice), singleRoomNumber);
-      addRoom(new DoubleRoom(doubleRoomPrice), doubleRoomNumber);
-      addRoom(new TripleRoom(tripleRoomPrice), tripleRoomNumber);
-      addRoom(new Apartment(apartmentPrice), apartmentNumber);
    }
 
    public void addRoom(Room r, int numberOfRooms)
@@ -63,6 +66,86 @@ public class Hotel
    public void setCity(String city)
    {
       this.city = city;
+   }
+   
+   public int getNumberOfSingleRooms()
+   {
+      return numberOfSingleRooms;
+   }
+
+   public void setNumberOfSingleRooms(int numberOfSingleRooms)
+   {
+      this.numberOfSingleRooms = numberOfSingleRooms;
+   }
+
+   public double getSingleRoomPrice()
+   {
+      return singleRoomPrice;
+   }
+
+   public void setSingleRoomPrice(double singleRoomPrice)
+   {
+      this.singleRoomPrice = singleRoomPrice;
+   }
+
+   public int getNumberOfDoubleRooms()
+   {
+      return numberOfDoubleRooms;
+   }
+
+   public void setNumberOfDoubleRooms(int numberOfDoubleRooms)
+   {
+      this.numberOfDoubleRooms = numberOfDoubleRooms;
+   }
+
+   public double getDoubleRoomPrice()
+   {
+      return doubleRoomPrice;
+   }
+
+   public void setDoubleRoomPrice(double doubleRoomPrice)
+   {
+      this.doubleRoomPrice = doubleRoomPrice;
+   }
+
+   public int getNumberOfTripleRooms()
+   {
+      return numberOfTripleRooms;
+   }
+
+   public void setNumberOfTripleRooms(int numberOfTripleRooms)
+   {
+      this.numberOfTripleRooms = numberOfTripleRooms;
+   }
+
+   public double getTripleRoomPrice()
+   {
+      return tripleRoomPrice;
+   }
+
+   public void setTripleRoomPrice(double tripleRoomPrice)
+   {
+      this.tripleRoomPrice = tripleRoomPrice;
+   }
+
+   public int getNumberOfApartments()
+   {
+      return numberOfApartments;
+   }
+
+   public void setNumberOfApartments(int numberOfApartments)
+   {
+      this.numberOfApartments = numberOfApartments;
+   }
+
+   public double getApartmentPrice()
+   {
+      return apartmentPrice;
+   }
+
+   public void setApartmentPrice(double apartmentPrice)
+   {
+      this.apartmentPrice = apartmentPrice;
    }
 
 }
