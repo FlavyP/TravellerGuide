52e7acc055f8fd2dbe824cc78c3050e8a8c28bae
Stabdis <gytis.kuosaitis@gmail.com>
2015-05-14 10:11:05 +0200

    hotelList added

---
+++ src/model/HotelList.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.util.ArrayList;
+
+public class HotelList
+{
+   
+   private ArrayList<Hotel> hotellist;
+
+   public HotelList()
+   {
+      hotellist = new ArrayList<Hotel>();
+   }
+   
+   public void addHotel(Hotel hotel)
+   {
+      hotellist.add(hotel);
+   }
+   
+   public int getHowManyHotels()
+   {
+      return hotellist.size();
+   }
+   
+   public ArrayList<Hotel> getHotelsInCity(String city)
+   {
+      ArrayList<Hotel> hotels = new ArrayList<Hotel>();
+      
+      for(int i = 0; i < hotellist.size(); i++)
+      {
+         if(hotellist.get(i).getCity().equals(city))
+         {
+            hotels.add(hotellist.get(i));
+         }
+      }
+      
+      return hotels;
+         
+   }
+   
+   public ArrayList<Hotel> getHotelsInAddress(String address)
+   {
+      ArrayList<Hotel> hotels = new ArrayList<Hotel>();
+      
+      for(int i = 0; i < hotellist.size(); i++)
+      {
+         if(hotellist.get(i).getAddress().equals(address))
+         {
+            hotels.add(hotellist.get(i));
+         }
+      }
+      
+      return hotels;
+   }
+}
