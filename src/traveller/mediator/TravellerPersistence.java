9bf096c1a701e7d6ddfe9800716ed1486dbc989b
mihaeladiaconescu <mihaeladiaconescu@Mihaela>
2015-05-17 23:11:40 +0200

    addReservation method

--- src/traveller/mediator/TravellerPersistence.java
+++ src/traveller/mediator/TravellerPersistence.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import traveller.model.Hotel;
 import traveller.model.HotelList;
+import traveller.model.Reservation;
 import traveller.model.User;
 import traveller.model.UserList;
 
@@ -14,6 +15,7 @@ public interface TravellerPersistence
    public UserList loadUsers() throws IOException;
    public int addHotel(Hotel hotel) throws IOException;
    public int addUser(User user) throws IOException;
+   public int addReservation(Reservation reservation) throws IOException;
    
 
 }
