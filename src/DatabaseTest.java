b659a50efe810e81a20c3a94a6fff96c260c7ec2
Julius <Julius@Julius-PC.lan>
2015-05-17 18:06:52 +0200

    edit

--- src/DatabaseTest.java
+++ src/DatabaseTest.java
@@ -6,13 +6,13 @@ public class DatabaseTest
 {
 
    private TravellerPersistence database;
-   //private HotelList hotellist;
+   private HotelList hotellist;
    private UserList userlist;
 
    public DatabaseTest() throws ClassNotFoundException, IOException
    {
       database = new TravellerDatabaseAdapter();
-      //hotellist = database.loadHotels();
+      hotellist = database.loadHotels();
       userlist = database.loadUsers();
    }
 
@@ -23,11 +23,11 @@ public class DatabaseTest
       DatabaseTest adaptertest = new DatabaseTest();
       adaptertest.test();
 
-
    }
-   
+
    public void test() throws IOException
    {
+      System.out.println(hotellist);
       System.out.println(userlist);
    }
 
--- src/traveller/model/Hotel.java
+++ src/traveller/model/Hotel.java
@@ -6,7 +6,7 @@ public class Hotel
    private String name;
    private String address;
    private String city;
-   
+
    public Hotel(String name, String city, String address,
          int numberOfSingleRooms, double singleRoomPrice,
          int numberOfDoubleRooms, double doubleRoomPrice,
@@ -16,19 +16,24 @@ public class Hotel
       this.name = name;
       this.address = address;
       this.city = city;
-//      roomList = new RoomList(numberOfSingleRooms, numberOfDoubleRooms, numberOfTripleRooms, numberOfApartments);
+      // roomList = new RoomList(numberOfSingleRooms, numberOfDoubleRooms,
+      // numberOfTripleRooms, numberOfApartments);
       roomList = new RoomList();
       roomList.setPriceOfRooms(0, singleRoomPrice);
       roomList.setPriceOfRooms(1, doubleRoomPrice);
       roomList.setPriceOfRooms(2, tripleRoomPrice);
       roomList.setPriceOfRooms(3, apartmentPrice);
-      roomList.addRooms(numberOfSingleRooms, numberOfDoubleRooms, numberOfTripleRooms, numberOfApartments);
-      
-      /*roomList.setNumberOfRooms(numberOfSingleRooms, 0);
-      roomList.setNumberOfRooms(numberOfDoubleRooms, 1);
-      roomList.setNumberOfRooms(numberOfTripleRooms, 2);
-      roomList.setNumberOfRooms(numberOfApartments, 3);*/
+      roomList.addRooms(numberOfSingleRooms, numberOfDoubleRooms,
+            numberOfTripleRooms, numberOfApartments);
+
+      /*
+       * roomList.setNumberOfRooms(numberOfSingleRooms, 0);
+       * roomList.setNumberOfRooms(numberOfDoubleRooms, 1);
+       * roomList.setNumberOfRooms(numberOfTripleRooms, 2);
+       * roomList.setNumberOfRooms(numberOfApartments, 3);
+       */
    }
+
    public String getName()
    {
       return name;
@@ -58,54 +63,73 @@ public class Hotel
    {
       this.city = city;
    }
-   
+
    public int getNumberOfRooms(int idx)
    {
       return roomList.getNumberOfRooms(idx);
    }
-   
+
    public double getPriceOfRooms(int idx)
    {
       return roomList.getPriceOfRooms(idx);
    }
-   
+
    public void setPriceOfRooms(int idx, double price)
    {
       roomList.setPriceOfRooms(idx, price);
    }
-   public int getRoomId(int index){
-	   return roomList.getId(index);
+
+   public int getRoomId(int index)
+   {
+      return roomList.getId(index);
    }
-   public Room getfirstAvailableSingleRoom(){
-	   return roomList.getFirstSingleRoom();
+
+   public Room getfirstAvailableSingleRoom()
+   {
+      return roomList.getFirstSingleRoom();
    }
-   public Room getfirstAvailableDoubleRoom(){
-	   return roomList.getFirstDoubleRoom();
+
+   public Room getfirstAvailableDoubleRoom()
+   {
+      return roomList.getFirstDoubleRoom();
    }
-   public Room getfirstAvailableTripleRoom(){
-	   return roomList.getFirstTripleRoom();
+
+   public Room getfirstAvailableTripleRoom()
+   {
+      return roomList.getFirstTripleRoom();
    }
-   public Room getfirstAvailableApartment(){
-	   return roomList.getFirstApartment();
+
+   public Room getfirstAvailableApartment()
+   {
+      return roomList.getFirstApartment();
    }
-   public void setAvailability(Room r, boolean isAv, MyDate checkIn, MyDate checkOut)
+
+   public void setAvailability(Room r, boolean isAv, MyDate checkIn,
+         MyDate checkOut)
    {
       roomList.setAvailability(r, isAv, checkIn, checkOut);
    }
-   
+
    public void markFreeRooms()
    {
       roomList.markFreeRooms();
    }
-   
+
    public Room getRoom(int index)
    {
       return roomList.getRoom(index);
    }
-   
-   
+
    public String toString()
    {
-      return "Name: " + name + "; Address: " + address + "; City: " + city;
+      return "Name: " + name + "; Address: " + address + "; City: " + city
+            + "\n" + "Number of Single Rooms: " + roomList.getNumberOfRooms(0)
+            + " Price of Single Rooms: " + roomList.getPriceOfRooms(0) + "\n"
+            + "Number of Double Rooms: " + roomList.getNumberOfRooms(1)
+            + " Price of Double Rooms: " + roomList.getPriceOfRooms(1) + "\n"
+            + "Number of Triple Rooms: " + roomList.getNumberOfRooms(2)
+            + " Price of Triple Rooms: " + roomList.getPriceOfRooms(2) + "\n"
+            + "Number of Apartments: " + roomList.getNumberOfRooms(3)
+            + " Price of Apartments: " + roomList.getPriceOfRooms(3);
    }
 }
