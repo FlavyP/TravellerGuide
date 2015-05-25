3dba9140b2354d5bb9abce0d6dd90095fb6952c5
Stabdis <gytis.kuosaitis@gmail.com>
2015-05-25 10:45:16 +0200

    deleted classes

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,25 +1,20 @@
-
-import traveller.controller.TravellerController;
+import traveller.mediator.ServerCommunicationThread;
+import traveller.mediator.ServerConnectionThread;
 import traveller.mediator.TravellerModelManager;
-import traveller.view.TravellerConsole;
-import traveller.view.TravellerView;
 
-public class MainServer
-{
-   public static void main(String[] args)
-   {
-      try
-      {
-         TravellerModelManager model = new TravellerModelManager();
-         TravellerView view = new TravellerConsole();
-         //DatabaseQueries db = new DatabaseQueries();
-         TravellerController controller = new TravellerController(model, view);
-         
-         view.start(controller);
-      }
-      catch (Exception e)
-      {
-         e.printStackTrace();
-      }
-   }
+public class MainServer {
+	public static void main(String[] args) {
+		try {
+			TravellerModelManager model = new TravellerModelManager();
+			// TravellerView view = new TravellerConsole();
+			// //DatabaseQueries db = new DatabaseQueries();
+			// TravellerController controller = new TravellerController(model,
+			// view);
+			//
+			// view.start(controller);
+			ServerConnectionThread server = new ServerConnectionThread(model);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
