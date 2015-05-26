bbbf3711fb6740d9ff0d9c87160aa54db7b2f013
Flavi <Flavi@192.168.1.13>
2015-05-26 20:10:29 +0200

    javadocs addded

--- src/traveller/model/DoubleRoom.java
+++ src/traveller/model/DoubleRoom.java
@@ -1,15 +1,34 @@
 package traveller.model;
 
+/**
+ * A class representing the Google Directions API.
+ * 
+ * @author Flavian Popa
+ * @author Gytis Kuosaitis
+ * @author Julius Jurgauskas
+ * @author Mihaela Diaconescu
+ * @version 1.0
+ */
+
 public class DoubleRoom extends Room{
 
 	private double price;
 	
+	/**
+	 * Two-argument constructor.
+	 * @param roomId room's id.
+	 * @param price room's price per night.
+	 */
 	public DoubleRoom(int roomId, double price)
 	{
 	   super(roomId);
 		this.price = price;
 	}
 	
+	/**
+	 * Gets room's price per night.
+	 * @return room's price per night.
+	 */
 	public double getPrice() {
 		return price;
 	}
--- src/traveller/model/TripleRoom.java
+++ src/traveller/model/TripleRoom.java
@@ -1,17 +1,36 @@
 package traveller.model;
 
+/**
+ * A class representing the Google Directions API.
+ * 
+ * @author Flavian Popa
+ * @author Gytis Kuosaitis
+ * @author Julius Jurgauskas
+ * @author Mihaela Diaconescu
+ * @version 1.0
+ */
+
 public class TripleRoom extends Room{
 
-	private double price;
-	
-	public TripleRoom(int roomId, double price)
-	{
-	   super(roomId);
-		this.price = price;
-	}
-	
-	public double getPrice() {
-		return price;
-	}
-	
+   private double price;
+   
+   /**
+    * Two-argument constructor.
+    * @param roomId room's id.
+    * @param price room's price per night.
+    */
+   public TripleRoom(int roomId, double price)
+   {
+      super(roomId);
+      this.price = price;
+   }
+   
+   /**
+    * Gets room's price per night.
+    * @return room's price per night.
+    */
+   public double getPrice() {
+      return price;
+   }
+   
 }
