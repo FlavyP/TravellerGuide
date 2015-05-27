18f52de478e4bd8502b977b53cc72e9ef78a5f0b
Julius <Julius@10.52.228.255>
2015-05-27 11:30:13 +0200

    review docs

--- src/traveller/model/Review.java
+++ src/traveller/model/Review.java
@@ -1,11 +1,29 @@
 package traveller.model;
 
+/**
+* A class representing a hotel.
+* @author Flavian Popa
+* @author Gytis Kuosaitis
+* @author Julius Jurgauskas
+* @author Mihaela Diaconescu
+* @version 1.0
+*/
+
 public class Review {
 	private int grade;
 	private String comment;
 	private int revId;
 	private User user;
 	private Hotel hotel;
+	
+	/**
+	   * Five-argument constructor.
+	   * @param revId reservation's id
+	   * @param user user's object
+	   * @param hotel hotel's object
+	   * @param grade given grade of review
+	   * @param comment review's comment
+	   */
 
 	public Review(int revId, User user, Hotel hotel, int grade, String comment) {
 		this.grade = grade;
@@ -14,43 +32,73 @@ public class Review {
 		this.user = user;
 		this.hotel = hotel;
 	}
-
+   /**
+   * Gets the review's grade.
+   * @return the review's grade.
+   */
 	public int getGrade() {
 		return grade;
 	}
-
+   /**
+   * Sets the grade.
+   * @param grade what the review grade will be
+   */
 	public void setGrade(int grade) {
 		this.grade = grade;
 	}
-
+	/**
+   * Gets the review's comment.
+   * @return the review's comment.
+	*/
 	public String getComment() {
 		return comment;
 	}
-
+   /**
+   * Sets the comment.
+   * @param comment what the review comment will be
+   */
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
-
+	/**
+   * Gets the reservation's id.
+   * @return the reservation's id.
+   */
 	public int getRevId() {
 		return revId;
 	}
-
+   /**
+   * Sets the reservation id.
+   * @param revId what reservation id you assign
+   */
 	public void setRevId(int revId) {
 		this.revId = revId;
 	}
-
+   /**
+   * Gets the user.
+   * @return the user's object.
+   */
 	public User getUser() {
 		return user;
 	}
-
+   /**
+   * Sets the user object.
+   * @param user which object you assign to user
+   */
 	public void setUser(User user) {
 		this.user = user;
 	}
-
+   /**
+   * Gets the hotel.
+   * @return the hotel's object.
+   */
 	public Hotel getHotel() {
 		return hotel;
 	}
-
+   /**
+   * Sets the hotel object.
+   * @param hotel which object you assign to hotel
+   */
 	public void setHotel(Hotel hotel) {
 		this.hotel = hotel;
 	}
--- src/traveller/model/ReviewList.java
+++ src/traveller/model/ReviewList.java
@@ -2,25 +2,47 @@ package traveller.model;
 
 import java.util.ArrayList;
 
+/**
+ * A class representing a hotel.
+ * @author Flavian Popa
+ * @author Gytis Kuosaitis
+ * @author Julius Jurgauskas
+ * @author Mihaela Diaconescu
+ * @version 1.0
+ */
+
 public class ReviewList
 {
    private ArrayList<Review> reviews;
    
+   /**
+    * No-argument constructor
+    */
    public ReviewList()
    {
       reviews = new ArrayList<Review>();
    }
-   
+   /**
+    * Adds review to the review list
+    * @param hotel what review will be added
+    */
    public void addReview(Review r)
    {
       reviews.add(r);
    }
-   
+   /**
+    * Deletes review from the review list
+    * @param index shows what review with index will be deleted
+    */
    public void deleteReview(int index)
    {
       reviews.remove(index);
    }
-   
+   /**
+    * Gets review list by user id
+    * @param userId what reviews will be selected
+    * @return review list by user id
+    */
    public ArrayList<Review> getReviews(int userId)
    {
       ArrayList<Review> newRev = new ArrayList<Review>();
@@ -33,7 +55,11 @@ public class ReviewList
       }
       return newRev;
    }
-   
+   /**
+    * Gets review list by hotel id
+    * @param hotelId what reviews will be selected
+    * @return review list by hotel id
+    */
    public ArrayList<Review> getReviewsByHotel(int hotelId)
    {
       ArrayList<Review> newRev = new ArrayList<Review>();
@@ -46,7 +72,10 @@ public class ReviewList
       }
       return newRev;
    }
-   
+   /**
+    * Gets average grade
+    * @return average grade
+    */
    public double getAverage()
    {
       double average = 0;
@@ -56,7 +85,10 @@ public class ReviewList
       }
       return average / reviews.size();
    }
-   
+   /**
+    * Gets the review list size
+    * @return the review list size
+    */
    public int size(){
 	   return reviews.size();
    }
