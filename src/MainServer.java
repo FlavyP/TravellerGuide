a133055ab621a45ad8e93ea004acd9b16f54af4b
Stabdis <gytis.kuosaitis@gmail.com>
2015-05-28 13:40:29 +0200

    server frame

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,17 +1,13 @@
 import traveller.mediator.ServerCommunicationThread;
 import traveller.mediator.ServerConnectionThread;
 import traveller.mediator.TravellerModelManager;
+import traveller.view.ServerFrame;
 
 public class MainServer {
 	public static void main(String[] args) {
 		try {
 			TravellerModelManager model = new TravellerModelManager();
-			// TravellerView view = new TravellerConsole();
-			// //DatabaseQueries db = new DatabaseQueries();
-			// TravellerController controller = new TravellerController(model,
-			// view);
-			//
-			// view.start(controller);
+			ServerFrame.getInstance();
 			ServerConnectionThread server = new ServerConnectionThread(model);
 		} catch (Exception e) {
 			e.printStackTrace();
